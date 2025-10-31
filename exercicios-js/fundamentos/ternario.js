e0d52be511c6d47b61796e73b2b75deb546d1b58
Luiz Oliveira <luiz.op17@gmail.com>
2025-10-31 20:02:58 -0300

    Adicionando descrição na função ternária

--- exercicios-js/fundamentos/ternario.js
+++ exercicios-js/fundamentos/ternario.js
@@ -1,4 +1,4 @@
-const resultado = nota => nota >= 7 ? 'Aprovado': 'Reprovado'
+const resultado = nota => nota >= 7 ? 'Aprovado': 'Reprovado' //parâmetro => condição ? if : else(retorno)
 console.log(resultado(5))
 console.log(resultado())
 console.log(resultado(7.1))
