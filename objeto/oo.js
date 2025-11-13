4d2d67a8c185f43b5c137154e4cc258019e35297
Luiz Oliveira <luiz.op17@gmail.com>
2025-11-13 20:13:56 -0300

    Princípios de O.O. e criação de Objetos

---
+++ objeto/oo.js
@@ -0,0 +1,21 @@
+//Código não executável
+
+//Procedural
+Processamento(valor1, valor2, valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() //Foco passou a ser objeto
+//Principios importantes:
+//1. abstração
+//2. encapsulamento
+//3. herança (prototype)... "é um"
+//4. polimorfismo -> Muntiplas formas
